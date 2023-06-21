e5589292896f449e067c889d425d526a43b6b737
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-06-22 00:13:53 +0530

    [Niketan]Add:Welcome to Line COmmparision Problem

---
+++ Line_Comparision_Problems/Line_Comparision_Problems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision_Problems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***************Welcome to Line Comparision Problems*****************");
+            
+            Console.ReadLine();
+        }
+    }
+}
